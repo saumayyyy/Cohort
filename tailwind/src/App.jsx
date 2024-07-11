a16f294b80f07b6bb5cde823c3eac14f816bbf78
Saumayyyy <saumayaggarwal6@gmail.com>
2024-07-11 06:12:07 +0530

    tailwind

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,32 @@
+
+
+
+function App(){
+
+  return (
+    <div className="relative">
+      <div className="md:flex-row h-[300px] w-[100vw] bg-slate-500 flex
+        flex-col justify-center gap-[20px] items-center">
+          <div className="h-[100px] w-[100px] bg-white flex justify-center items-center">Saumayyy</div>
+          <div className="h-[100px] w-[100px] bg-white flex justify-center items-center">Saumayyy</div>
+          <div className="h-[100px] w-[100px] bg-white flex justify-center items-center">Saumayyy</div>
+          <div className="h-[100px] w-[100px] bg-white flex justify-center items-center">Saumayyy</div>
+          <div className="h-[100px] w-[100px] bg-white flex justify-center items-center">Saumayyy</div>
+    </div>
+    <div className=" mt-[20px] grid grid-cols-4 absolute h-full w-full z-10 p-10 gap-5">
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+      <div className="bg-amber-300 h-full w-full border-zinc-700 border-solid border-8 md:bg-red-600"></div>
+    </div>
+  </div>
+  )
+}
+
+
+export default App;
